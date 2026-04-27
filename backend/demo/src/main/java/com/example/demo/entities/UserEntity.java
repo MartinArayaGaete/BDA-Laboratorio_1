7587719df2f112a3690281788050287e9f6d2eff
Joseardi <jose.ceardi@usach.cl>
2026-04-27 19:19:04 -0400

    Fix schema.sql and data.sql; the last commit didn’t update them, which is causing a crash.

--- backend/demo/src/main/java/com/example/demo/entities/UserEntity.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.entities;
-
-import lombok.Data;
-
-@Data
-public class UserEntity {
-
-}
--- backend/demo/src/main/resources/data.sql
+++ backend/demo/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-
 INSERT INTO usuario (rut, nombre, correo, contrasena, rol)
-VALUES ('11111111-1', 'Juan Perez', 'juan@mail.com', 'hola123', 'ADMIN');
+VALUES ('11111111-1', 'Juan Perez', 'juan@mail.com', 'hola123', 'ADMIN')
+ON CONFLICT (rut) DO NOTHING;
