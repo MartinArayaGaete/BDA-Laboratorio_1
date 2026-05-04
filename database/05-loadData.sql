124293474c3e98f5067a4c0d6d3804d57523b4d8
MartinAlvayay <martin.alvayay@usach.cl>
2026-05-03 20:24:14 -0400

    Nuevo LoadData sql Database

--- database/05-loadData.sql
+++ database/05-loadData.sql
@@ -15,71 +15,87 @@ INSERT INTO usuario (rut, nombre, correo, contrasena, rol) VALUES
 
 -- 3. TORNEO (ahora con IN_COURSE)
 INSERT INTO torneo (id_categoria, nombre_torneo, estado_torneo, fecha_inicio, fecha_termino) VALUES
-(1, 'Copa Apertura', 'COMPLETED', '2026-02-08', '2026-02-09'),
-(2, 'Open Valle',    'COMPLETED', '2026-02-22', '2026-02-23'),
-(3, 'Torneo Costa',  'COMPLETED', '2026-03-15', '2026-03-16'),
-(1, 'Ranking Otoño', 'COMPLETED', '2026-04-12', '2026-04-13'),
-(2, 'Gran Premio',   'COMPLETED', '2026-04-26', '2026-04-28'),
-(3, 'Copa Invierno', 'NOT_STARTED','2026-06-04','2026-06-06'),
-
--- NUEVOS EN CURSO
-(1, 'Liga Metropolitana', 'IN_COURSE', '2026-05-01', '2026-05-10'),
-(2, 'Open Primavera',     'IN_COURSE', '2026-05-03', '2026-05-12');
+(1, 'Copa Apertura', 'IN_COURSE', '2026-02-08', '2026-02-09'),
+(2, 'Open Valle',    'IN_COURSE', '2026-02-22', '2026-02-23'),
+(3, 'Torneo Costa',  'IN_COURSE', '2026-03-15', '2026-03-16');
 
 -- 4. PARTICIPACION
 INSERT INTO participacion (id_usuario, id_torneo, puntaje_final, posicion_final) VALUES
--- COMPLETED
-(2,1,56,1),(3,1,53,2),(4,1,49,3),
-(3,2,55,1),(5,2,55,2),(4,2,48,3),
-(6,3,56,1),(2,3,53,2),(5,3,47,3),
-(6,4,84,1),(2,4,83,2),(3,4,74,3),
-(5,5,85,1),(3,5,83,2),(4,5,64,3),
 
--- IN_COURSE (sin puntaje final aún)
-(2,7,NULL,NULL),
-(3,7,NULL,NULL),
-(4,7,NULL,NULL),
+-- Torneos 1
+(2,1,NULL,NULL),
+(3,1,NULL,NULL),
+(4,1,NULL,NULL),
 
-(5,8,NULL,NULL),
-(6,8,NULL,NULL),
-(2,8,NULL,NULL);
+-- Torneos 2
+(5,2,NULL,NULL),
+(6,2,NULL,NULL),
+(2,2,NULL,NULL),
+
+-- Torneos 3
+(2,3,NULL,NULL),
+(4,3,NULL,NULL),
+(5,3,NULL,NULL);
+
 
 -- 5. RONDA
 INSERT INTO ronda (id_torneo, numero_ronda) VALUES
--- COMPLETED
-(1,1),(1,2),
-(2,1),(2,2),
-(3,1),(3,2),
-(4,1),(4,2),(4,3),
-(5,1),(5,2),(5,3),
+-- Torneo 1
+(1,1),(1,2), (1,3),
+-- Torneo 2
+(2,1),(2,2), (2,3),
+-- Torneo 3
+(3,1),(3,2), (3,3);
 
--- IN COURSE
-(7,1),(7,2),
-(8,1),(8,2);
+-- 6. Procedimiento almacenado
 
--- 6. PUNTAJE_RONDA (solo para completed)
-INSERT INTO puntaje_ronda (id_ronda, id_participacion, puntaje_ronda) VALUES
-(1,1,28),(1,2,25),(1,3,23),
-(2,1,28),(2,2,28),(2,3,26),
+-- Torneo 1 - Ronda 1
+CALL registrar_puntaje_ronda(1,1,ARRAY[3,0,1,2,0,6],1);
+CALL registrar_puntaje_ronda(1,2,ARRAY[0,5,1,2,3,10],1);
+CALL registrar_puntaje_ronda(1,3,ARRAY[1,0,1,0,10,0],1);
 
-(3,4,29),(3,5,23),(3,6,26),
-(4,4,26),(4,5,25),(4,6,29);
+-- Torneo 1 - Ronda 2
+CALL registrar_puntaje_ronda(2,1,ARRAY[7,0,9,7,0,10],1);
+CALL registrar_puntaje_ronda(2,2,ARRAY[9,0,5,7,2,0],1);
+CALL registrar_puntaje_ronda(2,3,ARRAY[10,0,7,10,0,10],1);
 
--- 7. FLECHA
-INSERT INTO flecha (id_puntaje_ronda, puntaje) VALUES
-(1,10),(1,9),(1,9),
-(2,9),(2,8),(2,8),
-(3,7),(3,8),(3,8);
+-- Torneo 1 - Ronda 3 
+CALL registrar_puntaje_ronda(3,1,ARRAY[0,6,8,10,0,2],1);
+CALL registrar_puntaje_ronda(3,2,ARRAY[6,0,5,7,0,3],1);
+CALL registrar_puntaje_ronda(3,3,ARRAY[0,4,6,1,0,3],1);
 
--- 8. LOGS
-INSERT INTO logs (
-    id_admin,
-    id_afectado,
-    fecha_editado,
-    torneo_afectado,
-    ronda_afectada,
-    puntaje_anterior,
-    puntaje_nuevo
-) VALUES
-(1,2,'2026-02-09 15:30:00',1,1,27,28),
-(1,3,'2026-02-23 16:10:00',2,1,28,29);
+-- Torneo 2 - Ronda 1
+CALL registrar_puntaje_ronda(1,4,ARRAY[7,0,2,0,5,6],1);
+CALL registrar_puntaje_ronda(1,5,ARRAY[0,2,5,5,0,1],1);
+CALL registrar_puntaje_ronda(1,6,ARRAY[3,0,5,6,0,5],1);
+
+-- Torneo 2 - Ronda 2
+CALL registrar_puntaje_ronda(2,4,ARRAY[6,0,2,5,10,0],1);
+CALL registrar_puntaje_ronda(2,5,ARRAY[7,0,2,4,0,1],1);
+CALL registrar_puntaje_ronda(2,6,ARRAY[0,5,6,0,10,4],1);
+
+-- Torneo 2 - Ronda 3
+CALL registrar_puntaje_ronda(3,4,ARRAY[6,0,2,2,0,6],1);
+CALL registrar_puntaje_ronda(3,5,ARRAY[8,0,1,6,0,4],1);
+CALL registrar_puntaje_ronda(3,6,ARRAY[4,0,7,5,0,10],1);
+
+-- Torneo 3 - Ronda 1
+CALL registrar_puntaje_ronda(1,7,ARRAY[0,3,0,2,3,0],1);
+CALL registrar_puntaje_ronda(1,8,ARRAY[10,0,1,1,0,6],1);
+CALL registrar_puntaje_ronda(1,9,ARRAY[6,0,2,5,0,5],1);
+
+-- Torneo 3 - Ronda 2
+CALL registrar_puntaje_ronda(2,7,ARRAY[6,0,3,4,0,1],1);
+CALL registrar_puntaje_ronda(2,8,ARRAY[3,0,0,5,0,5],1);
+CALL registrar_puntaje_ronda(2,9,ARRAY[7,10,0,5,0,1],1);
+
+-- Torneo 3 - Ronda 3
+CALL registrar_puntaje_ronda(3,7,ARRAY[7,0,2,7,0,2],1);
+CALL registrar_puntaje_ronda(3,8,ARRAY[5,0,3,2,0,3],1);
+CALL registrar_puntaje_ronda(3,9,ARRAY[10,10,0,3,0,5],1);
+
+-- 7. Completacion de torneos
+UPDATE torneo SET estado_torneo = 'COMPLETED' WHERE id_torneo = 1;
+
+-- 8. Calcular posiciones finales (ejecutar procedimiento)
+CALL actualizar_posiciones(1);
